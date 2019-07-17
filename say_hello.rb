4163679cc3c5fd54957ba822cfcaa6ca3053d9e2
diegojctorres <diegojctorres@gmail.com>
2019-07-17 23:43:39 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,5 @@
 # Build your say_hello method here
-def say_hello(name = "Ruby programmer")
+def say_hello(name, name = "Ruby programmer")
   puts "Hello #{name}!"
 end
 say_hello("Kent Beck")
-end 
-
-def say_hello(name = "Ruby programmer")
-    puts "Hello #{name}!"
-end
